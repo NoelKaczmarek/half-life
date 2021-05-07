5c8b758d1d3da01ce23abf12160ddbba3cf9da74
NoelKaczmarek <noel.kaczmarek@gmail.com>
2021-05-07 11:43:06 +0200

    Add calc_all staticmethod to HalfLifeCalculator

--- main.py
+++ main.py
@@ -22,6 +22,17 @@ class HalfLifeCalculator(object):
     def calc_remaining(substance: Substance, t: float) -> float:
         return substance.initial_mass * math.exp(-t / substance.mean_lifetime)
 
+    @staticmethod
+    def calc_all(substance: Substance, t: float, step_size: float = 1) -> list:
+        values = []
+        steps = round(t / step_size)
+        current_t = 0
+
+        for step in range(steps):
+            values.append(substance.initial_mass * (2 ** (-(current_t / substance.mean_lifetime))))
+            current_t += step_size
+
+        return values
 
 def main():
     N0 = float(input('Initial mass of the carbon in kg='))
