e6ed395fce29b76411eb5efedee0d688bfb50f25
NoelKaczmarek <noel.kaczmarek@gmail.com>
2021-05-07 11:53:25 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,11 +1,13 @@
+from half_life.visualizer import Visualizer
 from half_life.calculator import HalfLifeCalculator
+from half_life.visualizer import Visualizer
 from half_life.substance import Substance
 
 
 def main():
     francium = Substance(600, 10)
     values = HalfLifeCalculator.calc_all(francium, 3600)
-    HalfLifeCalculator.show_graph(values)
+    Visualizer.show_graph(values)
 	
 
 if __name__ == '__main__':
