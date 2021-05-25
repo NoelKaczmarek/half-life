b351e838ad2fccb5187d6d1c562772918b87211a
Noel Kaczmarek <noel.kaczmarek@gmail.com>
2021-05-25 16:17:22 +0200

    Add package metadata to __init__.py

--- half_life/__init__.py
+++ half_life/__init__.py
@@ -2,3 +2,12 @@ import sys
 
 MEIPASS = sys._MEIPASS if hasattr(sys, '_MEIPASS') else None
 RESOURCE_PATH = MEIPASS or '.'
+
+__author__ = 'Noel Kaczmarek'
+__copyright__ = '© 2021 Noel Kaczmarek'
+__credits__ = ['Noel Kaczmarek']
+__license__ = 'GNU GPLv3'
+__version__ = '1.1.0'
+__maintainer__ = 'Noel Kaczmarek'
+__email__ = 'noel@kaczmarek.eu'
+__status__ = 'Production'
