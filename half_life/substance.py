7b2b9804d7e6bf0d2fab1fc50299de199979112e
Noel Kaczmarek <noel.kaczmarek@gmail.com>
2021-05-25 15:03:35 +0200

    Add decay constant member to Substance

--- half_life/substance.py
+++ half_life/substance.py
@@ -1,8 +1,12 @@
+import math
+
+
 class Substance(object):
     def __init__(self, half_life: float, initial_mass: float):
         self._mean_lifetime = None
         self._half_life = half_life
         self._initial_mass = initial_mass
+        self._decay_constant = math.log(2) / self._half_life
 
     @property
     def half_life(self) -> float:
@@ -16,6 +20,10 @@ class Substance(object):
     @property
     def initial_mass(self) -> float:
         return self._initial_mass
+
+    @property
+    def decay_constant(self) -> float:
+        return self._decay_constant
 
 
 all = [
